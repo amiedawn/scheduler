13588cfbe3a8ff1580ef6f55d310bdf6b88b2a25
amiedawn <amie.d.carroll@gmail.com>
2020-10-23 09:16:46 -0600

    colors applied for time of day

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,38 @@ var $headerDate = $("#currentDay");
 
 $headerDate.text(today);
 
+
+//time input from user in military time
+var apptTime = ["9", "10", "11", "12", "13", "14", "15", "16"];
+var now = moment().format("H");
+
+
+// color-code time-blocks based on comparison with time of day
+function compareTime() {
+  for (var i = 0; i < apptTime.length; i++) {
+    console.log("apptTime loop", apptTime[i], typeof apptTime[i]);
+    console.log("now loop", now, typeof now);
+    if (parseInt(apptTime[i]) < now) {
+      $("#" + apptTime[i]).attr("style", "background-color: gray");
+      console.log("apptTime", apptTime[i], typeof apptTime[i]);
+      console.log("now", now, typeof now);
+    } else if 
+       ("#" + parseInt(apptTime[i]) > now) {
+      $(apptTime[i]).attr("style", "background-color: green");
+      console.log("apptTime", apptTime[i], typeof apptTime[i]);
+      console.log("now", now, typeof now);
+    } else {
+      $("#" + apptTime[i]).attr("style", "background-color: #FF8484");
+    }
+  }  
+}
+
+
+compareTime();
+//if (today === 
+
+
+
+
+
+
