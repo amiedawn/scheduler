ee800603aa3cb5189a4dad6a575b4781bd15f53e
amiedawn <amie.d.carroll@gmail.com>
2020-10-23 16:10:31 -0600

    working on saving and local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,31 +14,55 @@ var nowInt = parseInt(now);
 
 // color-code time-blocks based on comparison with time of day
 function compareTime() {
+  // go through each time block and compare it with the current time
   for (var i = 0; i < splitApptTime.length; i++) {
-    console.log("splitApptTime loop", splitApptTime[i], typeof splitApptTime);
-    console.log("nowInt loop", nowInt, typeof nowInt);
+
+    // convert apptTime to number so it can be compared
     var apptTimeInt = parseInt(splitApptTime[i]);
+    
+    // past appts are gray
     if (apptTimeInt < nowInt) {
-      $("#" + apptTimeInt).attr("style", "background-color: gray");
-      console.log("apptTimeInt", apptTimeInt, typeof apptTimeInt);
-      console.log("nowInt", nowInt, typeof nowInt);
-    } else if
+      $("#" + apptTimeInt).attr("style", "background-color: #b5b2b2");
+      } else if
+      //current appts are red
       (apptTimeInt === nowInt) {
       $("#" + apptTimeInt).attr("style", "background-color: #FF8484");
-      console.log("apptTimeInt", apptTimeInt, typeof apptTimeInt);
-      console.log("nowInt", nowInt, typeof nowInt);
     } else if 
+      //future appts are green
        (apptTimeInt > nowInt) {
       $("#" + apptTimeInt).attr("style", "background-color: #70d470");
-      console.log("apptTimeInt", apptTimeInt, typeof apptTimeInt);
-      console.log("nowInt", nowInt, typeof nowInt);
     }
   }  
-}
+};
+
+
+
+// save buttons and save to local storage
+$(".saveBtn").on("click", function() {
+  
+  var currentTime = $(this).parent().attr("id");
+  var apptDescription = $(this).val().trim("id");
+
+  // save and set appt time
+  localStorage.setItem("currentTime",$("#apptDescription").text());
+  console.log("currentTime", currentTime);
+  console.log("apptDescription", apptDescription);
+});
+
+// store apppointment information
+$("#9am").children("input").val(localStorage.getItem("9am"));
+$("#10am").children("input").val(localStorage.getItem("10am"));
+$("#11am").children("input").val(localStorage.getItem("11am"));
+$("#12pm").children("input").val(localStorage.getItem("12pm"));
+$("#1pm").children("input").val(localStorage.getItem("1pm"));
+$("#2pm").children("input").val(localStorage.getItem("2pm"));
+$("#3pm").children("input").val(localStorage.getItem("3pm"));
+$("#4pm").children("input").val(localStorage.getItem("4pm"));
+$("#5pm").children("input").val(localStorage.getItem("5pm"));
 
 
 compareTime();
-//if (today === 
+
 
 
 
